6870cff32cf721452f12660b1b6e133853377a37
相原 直也 <aiharanaoya@team-lab.com>
2022-08-15 00:28:26 +0900

    feat: サンプルコンポーネントを追加

---
+++ src/components/content.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import style from './content.module.scss';
+
+type Props = {
+  text: string;
+};
+
+export const Content: FC<Props> = ({ text }) => {
+  return (
+    <div className={style['main-content']}>
+      <p>{text}</p>
+      <p>{text}</p>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import style from './index.module.scss';
+import { Content } from '@/components/content';
 
 const Index: NextPage = () => {
   return (
-    <main className={style['main-content']}>
-      <h1 className={style['header']}>nextjs-template</h1>
-    </main>
+    <>
+      <Head>
+        <title>nextjs-template</title>
+      </Head>
+      <main className={style['main-content']}>
+        <h1 className={style['header']}>nextjs-template</h1>
+        <Content text={'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'} />
+        <Content text={'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'} />
+        <Content text={'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'} />
+        <Content text={'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'} />
+      </main>
+    </>
   );
 };
 
