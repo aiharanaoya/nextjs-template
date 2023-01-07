295db4bc93188a099af9e3b19eab6efe8dbede1c
相原 直也 <aiharanaoya@team-lab.com>
2023-01-07 18:08:34 +0900

    stylelint-rscss削除

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,6 @@ module.exports = {
   extends: [
     // SCSS基本設定
     'stylelint-config-standard-scss',
-    // RSCSS用StyleLint
-    'stylelint-rscss/config',
     // StyleLintとPrettierが干渉しないようにする（extendsの最後に記載する）
     'stylelint-config-prettier'
   ]
