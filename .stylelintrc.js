472ba85227d5a96e1c55ade55795fae00509aa68
相原 直也 <aiharanaoya@team-lab.com>
2023-01-09 23:59:33 +0900

    stylelint-config-recess-orderを入れた

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: [
     // SCSS基本設定
     'stylelint-config-standard-scss',
+    // CSSのプロパティをソートする
+    'stylelint-config-recess-order',
     // StyleLintとPrettierが干渉しないようにする（extendsの最後に記載する）
     'stylelint-config-prettier'
   ]
