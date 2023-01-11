661e20c1341ad1f968a038313e66a23e75e63bb2
相原 直也 <aiharanaoya@team-lab.com>
2023-01-11 21:40:38 +0900

    インポート修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@/assets/styles/globals.scss';
+import '@/styles/globals.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
